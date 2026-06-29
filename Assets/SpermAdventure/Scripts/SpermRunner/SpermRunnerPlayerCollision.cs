94c1745aac471787b8d106418fb3b9d7946ef760
徐英森 <96182289+angus839186@users.noreply.github.com>
2026-06-29 19:59:34 +0800

    換子宮模型

--- Assets/SpermAdventure/Scripts/SpermRunner/SpermRunnerPlayerCollision.cs
+++ Assets/SpermAdventure/Scripts/SpermRunner/SpermRunnerPlayerCollision.cs
@@ -4,22 +4,40 @@ using UnityEngine;
 public class SpermRunnerPlayerCollision : MonoBehaviour
 {
     [SerializeField] private SpermRunnerGameController gameController;
+    [SerializeField] private float hazardCheckRadius = 0.9f;
+    [SerializeField] private LayerMask hazardLayer;
+
+    private void Update()
+    {
+        if (Physics.CheckSphere(transform.position, hazardCheckRadius, hazardLayer, QueryTriggerInteraction.Collide))
+        {
+            gameController.Die();
+        }
+    }
 
     private void OnTriggerEnter(Collider other)
     {
+        if (other.GetComponentInParent<SpermRunnerHazard>() != null)
+        {
+            gameController.Die();
+            return;
+        }
 
         if (other.GetComponent<CenterPoint>() != null)
         {
             other.gameObject.SetActive(false);
             gameController.RevealGoals();
         }
-
     }
 
     private void OnControllerColliderHit(ControllerColliderHit hit)
     {
-        if (hit.collider.GetComponent<SpermRunnerHazard>() != null)
+        if (hit.collider.GetComponentInParent<SpermRunnerHazard>() != null)
+        {
             gameController.Die();
+            return;
+        }
+
         if (hit.collider.TryGetComponent(out SpermRunnerGoal goal))
         {
             gameController.ReachGoal(goal);
